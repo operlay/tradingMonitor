72155f0a5dbd3ccc1faa2c362773fd8a61299da3
operlay <967030@mail.ru>
2021-01-25 22:31:07 +0300

    first

---
+++ src/main/java/com/monitor/trading/TradingApplication.java
@@ -0,0 +1,13 @@
+package com.monitor.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// https://start.spring.io/
+@SpringBootApplication
+public class TradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradingApplication.class, args);
+	}
+
+}
